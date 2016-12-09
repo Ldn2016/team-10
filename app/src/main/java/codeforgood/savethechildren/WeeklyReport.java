c77aedaa6132bd2802400e49e19a0dad03d85bbb
wv207 <wv207@exeter.ac.uk>
2016-12-09 23:56:34 +0000

    Developed send function

--- app/src/main/java/codeforgood/savethechildren/WeeklyReport.java
+++ app/src/main/java/codeforgood/savethechildren/WeeklyReport.java
@@ -1,5 +1,13 @@
 package codeforgood.savethechildren;
 
+import android.content.res.Resources;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created by david on 09/12/2016.
  */
@@ -30,7 +38,31 @@ public class WeeklyReport
     public WeeklyReport() {}
 
     public void setNutritional1(String ageGroup, String totalChildrenAtBeginning) {
-        this.ageGroup = ageGroup;
-        this.totalChildrenAtBeginning = totalChildrenAtBeginning;
+        this.ageGroup = ageGroup + ',';
+        this.totalChildrenAtBeginning = totalChildrenAtBeginning +',';
+    }
+
+
+
+    public void sendData() {
+        String urlString = Resources.getSystem().getString(R.string.websever);
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            String toSend = ageGroup + totalChildrenAtBeginning + bmiBelow + muacBelow + oedemaBelow
+                    + relapse + reAdmissions + totalAdmissions + movedFromOTP + recovered + death
+                    + defaulterUnconfirmed + defaulterConfirmed + nonRecoveryMedicalReferral +
+                    nonRecoveryNonResponse + totalDischarges + other + totalOut + totalChildrenAtEnd;
+
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(toSend);
+            writer.close();
+
+        } catch (MalformedURLException e) {
+            // Yeah this is really bad but hackathon
+        } catch (IOException e) {
+            // Again really really bad
+        }
     }
 }
