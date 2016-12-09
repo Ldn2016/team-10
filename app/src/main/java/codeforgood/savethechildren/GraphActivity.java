ae2c7fdd131b019fb3bcb86af8cd01f80124b78d
wv207 <wv207@exeter.ac.uk>
2016-12-09 20:23:02 +0000

    Updated buttons

--- app/src/main/java/codeforgood/savethechildren/GraphActivity.java
+++ app/src/main/java/codeforgood/savethechildren/GraphActivity.java
@@ -1,7 +1,9 @@
 package codeforgood.savethechildren;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by User on 09/12/2016.
  */
-public class GraphActivity {
+public class GraphActivity extends AppCompatActivity {
 }
