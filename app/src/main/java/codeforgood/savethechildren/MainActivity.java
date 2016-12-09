b7eb04c00d2c9018a68b020bd9c3f28a0db89dd3
wv207 <wv207@exeter.ac.uk>
2016-12-09 20:12:08 +0000

    Added main menu

--- app/src/main/java/codeforgood/savethechildren/MainActivity.java
+++ app/src/main/java/codeforgood/savethechildren/MainActivity.java
@@ -1,7 +1,8 @@
 package codeforgood.savethechildren;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // test
+    }
+
+    public void openNutrition(View v) {
+
+    }
+
+    public void openStock(View v) {
+
+    }
+
+    public void openGraphs(View v) {
+
     }
 }
---
+++ app/src/main/java/codeforgood/savethechildren/NutritionActivity.java
@@ -0,0 +1,8 @@
+package codeforgood.savethechildren;
+
+/**
+ * Created by User on 09/12/2016.
+ */
+
+public class NutritionActivity {
+}
