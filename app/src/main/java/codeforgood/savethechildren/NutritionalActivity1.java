83732db3327a1c4b0dd16306a6f4c3479354ea23
wv207 <wv207@exeter.ac.uk>
2016-12-09 23:19:33 +0000

    fixed typo breaking dan's ide (hopefully)

--- app/src/main/java/codeforgood/savethechildren/NutritionalActivity1.java
+++ app/src/main/java/codeforgood/savethechildren/NutritionalActivity1.java
@@ -11,7 +11,7 @@ public class NutritionalActivity1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nutritiona1l);
+        setContentView(R.layout.activity_nutritional1);
     }
 
     public void onClick(Void v) {
