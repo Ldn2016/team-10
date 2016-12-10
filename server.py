dd0f164cc5d70d47654a5d5081b52e4a764be4d0
jah257 <jackuar2009@hotmail.co.uk>
2016-12-10 03:01:50 +0000

    MORE changes unsurprisingly

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ class StringGenerator(object):
 	def getInfo():
 		with urllib.request.urlopen('http://ec2-52-212-183-253.eu-west-1.compute.amazonaws.com:8080/getFile') as response:
 			html = response.read()
-			
+			return html
 if __name__ == '__main__':
 	cherrypy.config.update( {"server.socket_host": "0.0.0.0"} )
 	cherrypy.quickstart(StringGenerator())
