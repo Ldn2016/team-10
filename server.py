d6e508fe30749ecde09c90efab32e69c869c09ad
jah257 <jackuar2009@hotmail.co.uk>
2016-12-10 01:50:20 +0000

    Nice

--- server.py
+++ server.py
@@ -45,3 +45,4 @@ class StringGenerator(object):
 if __name__ == '__main__':
 	cherrypy.config.update( {"server.socket_host": "0.0.0.0"} )
 	cherrypy.quickstart(StringGenerator())
+	createSpreadsheet()
