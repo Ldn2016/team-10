2981caf874d3c752c0187239239f7421f383e930
dth209 <dth209@exeter.ac.uk>
2016-12-10 01:08:41 +0000

    Fully compiled WeeklyReport (I hope, if not apologies Venables sorry)

--- app/src/main/java/codeforgood/savethechildren/NutritionalActivity2.java
+++ app/src/main/java/codeforgood/savethechildren/NutritionalActivity2.java
@@ -1,13 +1,32 @@
 package codeforgood.savethechildren;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NutritionalActivity2 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutritional2);
+    }
+
+    public void onClick(Void v)
+    {
+        WeeklyReport report = new WeeklyReport();
+        Spinner spinner = (Spinner) findViewById(R.id.nut_1_spinner);
+        String age = spinner.getSelectedItem().toString();
+
+        EditText EText = (EditText) findViewById(R.id.total_beginning_children);
+        String children = EText.toString();
+
+        report.setNutritional1(age, children);
+
+        Intent intent = new Intent(this.getApplicationContext(), NutritionalActivity2.class);
+
     }
 }
--- app/src/main/java/codeforgood/savethechildren/WeeklyReport.java
+++ app/src/main/java/codeforgood/savethechildren/WeeklyReport.java
@@ -16,6 +16,9 @@ public class WeeklyReport
     private String reAdmissions;
     private String totalAdmissions;
     private String movedFromOTP;
+    private String otherG;
+    private String totalIn;
+    private String promotedToOTP;
     private String recovered;
     private String death;
     private String defaulterUnconfirmed;
@@ -23,14 +26,65 @@ public class WeeklyReport
     private String nonRecoveryMedicalReferral;
     private String nonRecoveryNonResponse;
     private String totalDischarges;
-    private String other;
+    private String otherO;
     private String totalOut;
     private String totalChildrenAtEnd;
 
     public WeeklyReport() {}
 
-    public void setNutritional1(String ageGroup, String totalChildrenAtBeginning) {
+    public void setNutritional1(String ageGroup, String totalChildrenAtBeginning)
+    {
         this.ageGroup = ageGroup;
         this.totalChildrenAtBeginning = totalChildrenAtBeginning;
     }
+
+    public void setNutritional2(String bmiBelow, String muacBelow, String oedemaBelow,
+                                String relapse, String reAdmissions)
+    {
+        this.bmiBelow = bmiBelow;
+        this.muacBelow = muacBelow;
+        this.oedemaBelow = oedemaBelow;
+        this.relapse = relapse;
+        this.reAdmissions = reAdmissions;
+        int totalAdmissionsInt = Integer.parseInt(bmiBelow) + Integer.parseInt(muacBelow) +
+                                 Integer.parseInt(oedemaBelow) + Integer.parseInt(relapse) +
+                                 Integer.parseInt(reAdmissions);
+        this.totalAdmissions = Integer.toString(totalAdmissionsInt);
+    }
+
+    public void setNutritional3(String movedFromOTP, String otherG)
+    {
+        this.movedFromOTP = movedFromOTP;
+        this.otherG = otherG;
+        int totalInInt = Integer.parseInt(totalAdmissions) + Integer.parseInt(otherG);
+        this.totalIn = Integer.toString(totalInInt);
+    }
+
+    public void setNutritional4(String promotedToOTP, String recovered, String death, String defaulterUnconfirmed,
+                                String defaulterConfirmed, String nonRecoveryMedicalReferral,
+                                String nonRecoveryNonResponse)
+    {
+        this.promotedToOTP = promotedToOTP;
+        this.recovered = recovered;
+        this.death = death;
+        this.defaulterUnconfirmed = defaulterUnconfirmed;
+        this.defaulterConfirmed = defaulterConfirmed;
+        this.nonRecoveryMedicalReferral = nonRecoveryMedicalReferral;
+        this.nonRecoveryNonResponse = nonRecoveryNonResponse;
+        int totalDischargesInt = Integer.parseInt(recovered) + Integer.parseInt(death) +
+                                 Integer.parseInt(defaulterUnconfirmed) + Integer.parseInt(defaulterConfirmed) +
+                                 Integer.parseInt(nonRecoveryMedicalReferral) + Integer.parseInt(nonRecoveryNonResponse);
+        this.totalDischarges = Integer.toString(totalDischargesInt);
+    }
+
+    public void setNutritional5(String otherO)
+    {
+        this.otherO = otherO;
+        this.totalOut = this.totalDischarges + otherO;
+        int totalChildrenAtEndInt = Integer.parseInt(totalChildrenAtBeginning) + Integer.parseInt(totalIn) -
+                                    Integer.parseInt(totalOut);
+
+        this.totalChildrenAtEnd = Integer.toString(totalChildrenAtEndInt);
+    }
+
 }
